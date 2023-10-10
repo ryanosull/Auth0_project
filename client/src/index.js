36c33786b7ba85780aa299d674280e66dc0f7a66
William Ryan OSullivan <wrosullivan@gmail.com>
2023-10-10 16:28:36 -0500

    refactoring - removed provider component, env setup

--- client/src/index.js
+++ client/src/index.js
@@ -4,10 +4,21 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { Auth0Provider } from '@auth0/auth0-react';
+
+const domain = process.env.REACT_APP_AUTH0_DOMAIN;
+const clientID = process.env.REACT_APP_AUTH0_CLIENT_ID;
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Auth0Provider
+    domain={domain}
+    clientID={clientID}
+    redirectUri={window.location.origin}
+    >
     <App />
+    </Auth0Provider>
   </React.StrictMode>
 );
 
