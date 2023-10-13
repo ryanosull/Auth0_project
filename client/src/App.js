640e4e852f3b76d798eecbea11d5aacffaedbd71
William Ryan OSullivan <wrosullivan@gmail.com>
2023-10-13 13:38:50 -0500

    app loading state

--- client/src/App.js
+++ client/src/App.js
@@ -3,9 +3,11 @@ import LoginButton from "./components/LoginButton";
 import LogoutButton from "./components/LogoutButton";
 import './styles/App.css'
 import Profile from "./components/Profile";
-
+import { useAuth0 } from "@auth0/auth0-react";
 
 function App() {
+
+  const {isLoading, error} = useAuth0();
 
   // const [count, setCount] = useState(0);
 
